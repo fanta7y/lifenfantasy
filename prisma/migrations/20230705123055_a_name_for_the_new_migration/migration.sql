09039b27a2314ed3366d5d3e5d8a52ada10654fe
Daniel <d.motrenko2010@gmail.com>
2023-07-05 17:46:30 +0300

    m

--- prisma/migrations/20230705123055_a_name_for_the_new_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `descr` to the `category` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `category` ADD COLUMN `descr` TEXT NOT NULL;
